2cca4a1e679ff53fd5c8b2df51288b6ed656791e
szuper1212856 <djbazsibuissnes@gmail.com>
2025-09-17 18:04:49 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7U-dQWlhnsb7JN7QoDGzTB4EyfPcCrDU",
+  authDomain: "creascola-leaderboard.firebaseapp.com",
+  databaseURL: "https://creascola-leaderboard-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "creascola-leaderboard",
+  storageBucket: "creascola-leaderboard.appspot.com",
+  messagingSenderId: "227185192602",
+  appId: "1:227185192602:web:e1a6953c07ac3aa888f923"
+};
